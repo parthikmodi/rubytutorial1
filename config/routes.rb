0de458d2a3d68140d8658fea77c9811b5fe586ef
parthikmodi1 <parthik.modi10@gmail.com>
2019-05-29 00:03:19 -0400

    add / to routing path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: "posts#index"
   get 'posts', to: "posts#index"
   get 'posts/new', to: "posts#new"
   get 'posts/:id/edit', to: "posts#edit_post", as: :edit
